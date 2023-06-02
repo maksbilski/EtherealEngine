c859e88c43f3e2ea34cec0cd00622625f812cf33
Maks Biski <maksde25@gmail.com>
2023-06-02 21:20:52 +0200

    Remove junk

--- cube.cpp
+++ cube.cpp
@@ -6,7 +6,6 @@
 #include <string>
 #include <sstream>
 
-<<<<<<< HEAD
 #include "src/engine/IndexBuffer.hpp"
 #include "src/engine/Renderer.hpp"
 #include "src/engine/Shader.hpp"
@@ -14,15 +13,14 @@
 #include "src/engine/VertexArray.hpp"
 #include "src/engine/VertexBuffer.hpp"
 #include "src/engine/VertexBufferLayout.hpp"
-=======
-#include "src/engine/Renderer.hpp"
-#include "src/engine/VertexBuffer.hpp"
+
 #include "src/engine/IndexBuffer.hpp"
-#include "src/engine/VertexArray.hpp"
+#include "src/engine/Renderer.hpp"
 #include "src/engine/Shader.hpp"
-#include "src/engine/VertexBufferLayout.hpp"
 #include "src/engine/Texture.hpp"
->>>>>>> 9cf35f08b3bc34bdd4608ed5b56f44d44d5ec3f5
+#include "src/engine/VertexArray.hpp"
+#include "src/engine/VertexBuffer.hpp"
+#include "src/engine/VertexBufferLayout.hpp"
 
 #include "src/vendor/glm/glm.hpp"
 #include "src/vendor/glm/gtc/matrix_transform.hpp"
@@ -44,117 +42,53 @@ int main(void)
       return -1;
     }
 
-    glfwMakeContextCurrent(window);
+    // clang-format off
+    float positions[] = {-50.0f, -50.0f, +0.0f, +0.0f,
+                         +50.0f, -50.0f, +1.0f, +0.0f,
+                         +50.0f, +50.0f, +1.0f, +1.0f,
+                         -50.0f, +50.0f, +0.0f, 1.0f};
 
-    glfwSwapInterval(1);
+    // clang-format on
 
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "Error!" << std::endl;
+    unsigned int indices[] = {0, 1, 2, 2, 3, 0};
+
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    VertexArray va;
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
+
+    IndexBuffer ib(indices, 6);
+
+    glm::mat4 proj = glm::ortho(0.0f, 1920.0f, 0.0f, 1080.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
+    Shader shader("resources/shaders/basic.shader");
+    shader.Bind();
+
+    shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+
+    Texture texture("resources/textures/senti.png");
+    texture.Bind();
+    shader.SetUniform1i("u_Texture", 0);
+
+    va.Unbind();
+    vb.Unbind();
+    ib.Unbind();
+    shader.Unbind();
+
+    Renderer renderer;
+
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 200, 0);
+
+    while (!glfwWindowShouldClose(window)) {
     }
-    {
-      float positions[] = {-50.0f, -50.0f, 0.0f,  0.0f,  50.0f, -50.0f,
-                           1.0f,   0.0f,   50.0f, 50.0f, 1.0f,  1.0f,
-                           -50.0f, 50.0f,  0.0f,  1.0f};
-
-      unsigned int indices[] = {0, 1, 2, 2, 3, 0};
-
-      GLCall(glEnable(GL_BLEND));
-      GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-      VertexArray va;
-      VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-      VertexBufferLayout layout;
-      layout.Push<float>(2);
-      layout.Push<float>(2);
-      va.AddBuffer(vb, layout);
-
-      IndexBuffer ib(indices, 6);
-
-      glm::mat4 proj = glm::ortho(0.0f, 1920.0f, 0.0f, 1080.0f, -1.0f, 1.0f);
-      glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-      Shader shader("resources/shaders/basic.shader");
-      shader.Bind();
-
-      shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-
-      Texture texture("resources/textures/senti.png");
-      texture.Bind();
-      shader.SetUniform1i("u_Texture", 0);
-
-      va.Unbind();
-      vb.Unbind();
-      ib.Unbind();
-      shader.Unbind();
-
-      Renderer renderer;
-
-      ImGui::CreateContext();
-      ImGui_ImplGlfwGL3_Init(window, true);
-      ImGui::StyleColorsDark();
-
-      glm::vec3 translationA(200, 200, 0);
-      glm::vec3 translationB(400, 200, 0);
-
-      float r = 0.0f;
-      float increment = 0.05f;
-
-      while (!glfwWindowShouldClose(window)) {
-        renderer.Clear();
-
-        ImGui_ImplGlfwGL3_NewFrame();
-
-        shader.Bind();
-
-        {
-          glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-          glm::mat4 mvp = proj * view * model;
-          shader.SetUniformMat4f("u_MVP", mvp);
-
-          renderer.Draw(va, ib, shader);
-        }
-
-        {
-          glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-          glm::mat4 mvp = proj * view * model;
-          shader.SetUniformMat4f("u_MVP", mvp);
-
-          renderer.Draw(va, ib, shader);
-        }
-
-        if (r > 1.0f)
-          increment = -0.05f;
-        else if (r < 0.0f)
-          increment = 0.05f;
-
-        r += increment;
-
-            {
-                ImGui::SliderFloat3(
-                    "Translation A", &translationA.x, 0.0f,
-                     1920.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-                ImGui::SliderFloat3(
-                    "Translation B", &translationB.x, 0.0f,
-                     1920.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 
-                            1000.0f / ImGui::GetIO().Framerate, 
-                            ImGui::GetIO().Framerate);
-            }
-
-        ImGui::Render();
-        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-      }
-    }
-
-    ImGui_ImplGlfwGL3_Shutdown();
-    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
