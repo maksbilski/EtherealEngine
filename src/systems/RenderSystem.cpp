f58a53dc8886e7debfd56f43d0625cc5f3c0d429
Maks Biski <maksde25@gmail.com>
2023-06-03 23:27:33 +0200

    Implement RenderSystem

--- src/systems/RenderSystem.cpp
+++ src/systems/RenderSystem.cpp
@@ -1,10 +1,20 @@
 #include "RenderSystem.hpp"
+#include "../components/CameraComponent.hpp"
 #include "../components/GraphicsComponent.hpp"
 #include "../components/TransformComponent.hpp"
+#include "../vendor/glm/gtc/matrix_transform.hpp"
 #include "Entity.hpp"
-#include <glm/gtc/matrix_transform.hpp>
 
-void RenderSystem::update(float deltaTime) {
+RenderSystem::RenderSystem(std::shared_ptr<Entity> player) : m_Player(player) {
+  float fov = glm::radians(45.0f);
+  float aspectRatio = 1920.0f / 1080.0f;
+  float near = 0.1f;
+  float far = 100.0f;
+  m_ProjectionMatrix = glm::perspective(fov, aspectRatio, near, far);
+  updateViewMatrix();
+}
+
+void RenderSystem::update() {
   for (const auto &entity : entities) {
     render(entity);
   }
@@ -16,16 +26,30 @@ void RenderSystem::render(const std::shared_ptr<Entity> &entity) {
   auto transform = entity->getComponent<TransformComponent>();
 
   // Utwórz MVP
-  glm::mat4 model = transform->createTransformMatrix();
-  glm::mat4 MVP = projectionMatrix * viewMatrix * model; // zmienne globalne
+  glm::mat4 model = transform.createTransformMatrix();
+  glm::mat4 MVP = m_ProjectionMatrix * m_ViewMatrix * model; // zmienne globalne
 
+  VertexArray va;
+  VertexBuffer vb(graphics.mesh.m_Vertices, 5 * 4 * sizeof(float));
+
+  VertexBufferLayout layout;
+  layout.Push<float>(3);
+  layout.Push<float>(2);
+  va.AddBuffer(vb, layout);
+
+  IndexBuffer ib(graphics.mesh.m_Indices, 6);
   // Aktywuj shader
-  graphics->shader->use();
+  graphics.shader.Bind();
+  graphics.texture.Bind();
 
   // Prześlij MVP do shadera
-  GLint mvpLocation = glGetUniformLocation(graphics->shader->getID(), "MVP");
-  glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+  graphics.shader.SetUniformMat4f("u_MVP", MVP);
+  GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
+};
 
-  // Renderuj obiekt
-  graphics->mesh->draw();
+void RenderSystem::updateViewMatrix() {
+  m_ViewMatrix =
+      glm::lookAt(m_Player->getComponent<CameraComponent>().getPosition(),
+                  m_Player->getComponent<CameraComponent>().getCameraLookVec(),
+                  glm::vec3(0.0f, 1.0f, 0.0f));
 }
--- src/systems/RenderSystem.hpp
+++ src/systems/RenderSystem.hpp
@@ -7,11 +7,17 @@
 class RenderSystem {
 private:
   std::vector<std::shared_ptr<Entity>> entities;
+  std::shared_ptr<Entity> m_Player;
+  glm::mat4 m_ProjectionMatrix;
+  glm::mat4 m_ViewMatrix;
 
   void render(const std::shared_ptr<Entity> &entity);
+  void updateViewMatrix();
 
 public:
+  RenderSystem(std::shared_ptr<Entity> player);
+  ~RenderSystem();
   void addEntity(const std::shared_ptr<Entity> &entity);
   void removeEntity(const std::shared_ptr<Entity> &entity);
-  void update(float deltaTime);
+  void update();
 };
