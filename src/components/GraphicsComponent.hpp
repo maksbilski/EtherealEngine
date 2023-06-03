eda13a41692b45fa7e66b212b00a1be9f6931d46
Maks Biski <maksde25@gmail.com>
2023-06-03 23:25:09 +0200

    Implement setters

--- src/components/GraphicsComponent.hpp
+++ src/components/GraphicsComponent.hpp
@@ -14,4 +14,11 @@ public:
   // Konstruktor, który przyjmuje referencje do meshu, tekstury i shadera
   GraphicsComponent(Mesh &mesh, Texture &texture, Shader &shader)
       : mesh(mesh), texture(texture), shader(shader) {}
+
+  // Setter functions
+  void setMesh(Mesh &newMesh) { mesh = newMesh; }
+
+  void setTexture(Texture &newTexture) { texture = newTexture; }
+
+  void setShader(Shader &newShader) { shader = newShader; }
 };
