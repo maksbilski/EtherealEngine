2d35402c5953bb34a244c1d6d2e5a3fa09540946
Maks Biski <maksde25@gmail.com>
2023-06-03 23:25:55 +0200

    Update TransformComponent

--- src/components/TransformComponent.cpp
+++ src/components/TransformComponent.cpp
@@ -6,14 +6,14 @@
 glm::mat4 TransformComponent::createTransformMatrix() const {
   glm::mat4 transform = glm::mat4(1.0f);
 
-  transform = glm::translate(transform, position);
-  transform =
-      glm::rotate(transform, glm::radians(rotation.x), glm::vec3(1, 0, 0));
-  transform =
-      glm::rotate(transform, glm::radians(rotation.y), glm::vec3(0, 1, 0));
-  transform =
-      glm::rotate(transform, glm::radians(rotation.z), glm::vec3(0, 0, 1));
-  transform = glm::scale(transform, scale);
+  transform = glm::translate(transform, m_Position);
+  transform = glm::rotate(transform, glm::radians(m_Rotation.x),
+                          glm::vec3(1.0f, 0.0f, 0.0f));
+  transform = glm::rotate(transform, glm::radians(m_Rotation.y),
+                          glm::vec3(0.0f, 1.0f, 0.0f));
+  transform = glm::rotate(transform, glm::radians(m_Rotation.z),
+                          glm::vec3(0.0f, 0.0f, 1.0f));
+  transform = glm::scale(transform, m_Scale);
 
   return transform;
 }
--- src/components/TransformComponent.hpp
+++ src/components/TransformComponent.hpp
@@ -5,15 +5,15 @@
 
 class TransformComponent {
 public:
-  glm::vec3 position;
-  glm::vec3 rotation;
-  glm::vec3 scale;
+  glm::vec3 m_Position;
+  glm::vec3 m_Rotation;
+  glm::vec3 m_Scale;
 
   // Konstruktor, który ustawia pozycję, obrót i skalę
   TransformComponent(const glm::vec3 &position = glm::vec3(0.0f),
                      const glm::vec3 &rotation = glm::vec3(0.0f),
                      const glm::vec3 &scale = glm::vec3(1.0f))
-      : position(position), rotation(rotation), scale(scale) {}
+      : m_Position(position), m_Rotation(rotation), m_Scale(scale) {}
 
   // Metoda do tworzenia macierzy transformacji
   glm::mat4 createTransformMatrix() const;
