b2d32a92fd8dad7c7742cd87ae91c9a8fd91d607
Maks Biski <maksde25@gmail.com>
2023-06-03 23:27:02 +0200

    Fix bug in entity

--- src/systems/Entity.hpp
+++ src/systems/Entity.hpp
@@ -1,18 +1,28 @@
 #pragma once
 #include "../engine/Shader.hpp"
 #include "../vendor/glm/glm.hpp"
+#include <any>
 #include <memory>
 #include <string>
+#include <typeindex>
 #include <vector>
 
 class Entity {
 public:
   Entity() {}
 
-  template <typename T> void addComponent(std::unique_ptr<T> component);
+  template <typename T> void addComponent(T component) {
+    components[std::type_index(typeid(T))] = std::any(component);
+  }
 
-  template <typename T> T *getComponent();
+  template <typename T> T getComponent() {
+    auto it = components.find(std::type_index(typeid(T)));
+    if (it == components.end()) {
+      throw std::runtime_error("Component doesn't exist");
+    }
+    return std::any_cast<T>(it->second);
+  }
 
 private:
-  std::unordered_map<std::string, std::unique_ptr<void>> components;
+  std::unordered_map<std::type_index, std::any> components;
 };
