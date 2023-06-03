5a11ac496f2b433f7dc889828037bb9323b1c1d8
mrozek <01178582@pw.edu.pl>
2023-06-03 14:33:14 +0200

    implement CameraComponent header and cpp

--- src/Objects/Components/CameraComponent.cpp
+++ src/Objects/Components/CameraComponent.cpp
@@ -1,4 +1,81 @@
 #include "CameraComponent.hpp"
 
 CameraComponent::CameraComponent(const glm::vec3 &position)
-    : m_Position(position) {}
+    : m_Position(position) {
+  m_CurrentLookAngleX = 0.0f;
+  m_CurrentLookAngleX = 0.0f;
+
+  computeWalkVectors();
+}
+
+CameraComponent::~CameraComponent() {}
+
+void CameraComponent::computeWalkVectors() {
+  m_MovementUpVec = glm::vec3(0.0f, 1.0f, 0.0f);
+  m_MovementForwardVec = glm::rotate(glm::vec3(0.0f, 0.0f, -1.0f),
+                                     m_CurrentLookAngleY, m_MovementUpVec);
+  m_MovementSidewayVec =
+      glm::normalize(glm::cross(m_MovementForwardVec, m_MovementUpVec));
+}
+
+glm::vec3 CameraComponent::getPosition() const { return this->m_Position; }
+
+glm::vec3 CameraComponent::getMovementForwardVec() const {
+  return this->m_MovementForwardVec;
+}
+
+glm::vec3 CameraComponent::getMovementSidewayVec() const {
+  return this->m_MovementSidewayVec;
+}
+
+glm::vec3 CameraComponent::getMovementUpVec() const {
+  return this->m_MovementUpVec;
+}
+
+glm::vec3 CameraComponent::getCameraForwardVec() const {
+  return this->m_CameraForwardVec;
+}
+
+glm::vec3 CameraComponent::getCameraSidewayVec() const {
+  return this->m_CameraSidewayVec;
+}
+
+glm::vec3 CameraComponent::getCameraUpVec() const {
+  return this->m_CameraUpVec;
+}
+
+glm::vec3 CameraComponent::getCameraLookVec() const {
+  return this->m_CameraLookVec;
+}
+
+void CameraComponent::setPosition(glm::vec3 newPosition) {
+  this->m_Position = newPosition;
+}
+
+void CameraComponent::setMovementForwardVec(glm::vec3 newMovementForwardVec) {
+  this->m_MovementForwardVec = newMovementForwardVec;
+}
+
+void CameraComponent::setMovementSidewayVec(glm::vec3 newMovementSidewayVec) {
+  this->m_MovementForwardVec = newMovementSidewayVec;
+}
+
+void CameraComponent::setMovementUpVec(glm::vec3 newMovementUpVec) {
+  this->m_MovementForwardVec = newMovementUpVec;
+}
+
+void CameraComponent::setCameraForwardVec(glm::vec3 newCameraForwardVec) {
+  this->m_MovementForwardVec = newCameraForwardVec;
+}
+
+void CameraComponent::setCameraSidewayVec(glm::vec3 newCameraSidewayVec) {
+  this->m_MovementForwardVec = newCameraSidewayVec;
+}
+
+void CameraComponent::setCameraUpVec(glm::vec3 newCameraUpVec) {
+  this->m_MovementForwardVec = newCameraUpVec;
+}
+
+void CameraComponent::setCameraLookVec(glm::vec3 newCameraLookVec) {
+  this->m_MovementForwardVec = newCameraLookVec;
+}
--- src/Objects/Components/CameraComponent.hpp
+++ src/Objects/Components/CameraComponent.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../../vendor/glm/glm.hpp"
+#include "../../vendor/glm/gtx/rotate_vector.hpp"
 #include "InputComponent.hpp"
 
 class CameraComponent {
 private:
+  float m_CurrentLookAngleX;
+  float m_CurrentLookAngleY;
+
   glm::vec3 m_Position;
   glm::vec3 m_MovementForwardVec;
   glm::vec3 m_MovementSidewayVec;
@@ -15,15 +19,10 @@ private:
   glm::vec3 m_CameraUpVec;
   glm::vec3 m_CameraLookVec;
 
-  float m_CurrentLookAngleX;
-  float m_CurrentLookAngleY;
-
 public:
   CameraComponent(const glm::vec3 &position);
   ~CameraComponent();
   void computeWalkVectors();
-  void computeCameraOrientation();
-  void update(float deltaTime, const InputComponent &userInput);
   glm::vec3 getPosition() const;
   glm::vec3 getMovementForwardVec() const;
   glm::vec3 getMovementSidewayVec() const;
@@ -31,5 +30,13 @@ public:
   glm::vec3 getCameraForwardVec() const;
   glm::vec3 getCameraSidewayVec() const;
   glm::vec3 getCameraUpVec() const;
-  glm::vec3 getCameraUpVec() const;
+  glm::vec3 getCameraLookVec() const;
+  void setPosition(glm::vec3 newPosition);
+  void setMovementForwardVec(glm::vec3 newMovementForwardVec);
+  void setMovementSidewayVec(glm::vec3 newMovementSidewayVec);
+  void setMovementUpVec(glm::vec3 newMovementUpVec);
+  void setCameraForwardVec(glm::vec3 newCameraForwardVec);
+  void setCameraSidewayVec(glm::vec3 newCameraSidewayVec);
+  void setCameraUpVec(glm::vec3 newCameraUpVec);
+  void setCameraLookVec(glm::vec3 newCameraLookVec);
 };
