a4e275488d26beb1ffd6ffec712c46d70d2295ab
Maks Biski <maksde25@gmail.com>
2023-06-04 16:16:28 +0200

    Change graphics component data members to just model and shader

--- src/components/GraphicsComponent.hpp
+++ src/components/GraphicsComponent.hpp
@@ -1,24 +1,21 @@
 #pragma once
 
-#include "../engine/Mesh.hpp"
+#include "../engine/Model.hpp"
 #include "../engine/Shader.hpp"
-#include "../engine/Texture.hpp"
 
 class GraphicsComponent {
 public:
   // Referencje do meshu, tekstury i shadera
-  Mesh &m_Mesh;
-  Texture &m_Texture;
+  Model &m_Model;
   Shader &m_Shader;
 
   // Konstruktor, który przyjmuje referencje do meshu, tekstury i shadera
-  GraphicsComponent(Mesh &mesh, Texture &texture, Shader &shader)
-      : m_Mesh(mesh), m_Texture(texture), m_Shader(shader) {}
+  GraphicsComponent(Model &model, Shader &shader)
+      : m_Model(model), m_Shader(shader) {}
 
   // Setter functions
-  void setMesh(Mesh &newMesh) { m_Mesh = newMesh; }
 
-  void setTexture(Texture &newTexture) { m_Texture = newTexture; }
+  void setModel(Model &newModel) { m_Model = newModel; }
 
   void setShader(Shader &newShader) { m_Shader = newShader; }
 };
