d07c376b56969cb7edcad8d065632d8e4806e285
Maks Biski <maksde25@gmail.com>
2023-06-04 17:00:56 +0200

    Change ResourceManager class so it stores shared_pointer to Model objects rather then mesh and texture

--- src/systems/ResourceManager.hpp
+++ src/systems/ResourceManager.hpp
@@ -16,13 +16,11 @@ public:
   ResourceManager();
   ~ResourceManager();
 
-  std::shared_ptr<Mesh> getMesh(EntityType type);
-  std::shared_ptr<Texture> getTexture(EntityType type);
+  std::shared_ptr<Model> getModel(EntityType type);
   std::pair<std::shared_ptr<Shader>, std::shared_ptr<Shader>> getShader(EntityType type);
 
 private:
-  std::unordered_map<EntityType, std::shared_ptr<Mesh>> m_Meshes;
-  std::unordered_map<EntityType, std::shared_ptr<Texture>> m_Textures;
+  std::unordered_map<EntityType, std::shared_ptr<Model>> m_Models;
   std::unordered_map<EntityType, std::pair<std::shared_ptr<Shader>, std::shared_ptr<Shader>>> m_Shaders;
 
   void loadResources();
@@ -35,12 +33,8 @@ ResourceManager::~ResourceManager() {
   // Zasoby zostaną automatycznie usunięte, gdy ostatni shared_ptr zostanie zniszczony
 }
 
-std::shared_ptr<Mesh> ResourceManager::getMesh(EntityType type) {
-  return m_Meshes[type];
-}
-
-std::shared_ptr<Texture> ResourceManager::getTexture(EntityType type) {
-  return m_Textures[type];
+std::shared_ptr<Model> ResourceManager::getModel(EntityType type) {
+  return m_Models[type];
 }
 
 std::pair<std::shared_ptr<Shader>, std::shared_ptr<Shader>> ResourceManager::getShader(EntityType type) {
