c786495b78ffa55c085aa7cf77409d1f9da338b7
Maks Biski <maksde25@gmail.com>
2023-06-04 17:00:25 +0200

    Add resource manager reference data member and change createEntity to be more expressive

--- src/systems/EntityFactory.hpp
+++ src/systems/EntityFactory.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "EntityManager.hpp"
+#include "ResourceManager.hpp"
 
 enum class EntityType { TERRAIN };
 
 class EntityFactory {
 private:
   EntityManager &m_EntityManager;
+  ResourceManager &m_ResourceManager;
   Entity m_NextEntity = 0;
 
-  Entity createEntity();
+  Entity getNewEntityId();
 
 public:
   EntityFactory(EntityManager &entityManager);
