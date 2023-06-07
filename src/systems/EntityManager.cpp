4130691db5ceec89ff4112b11edcc269f303c6b6
Maks Biski <maksde25@gmail.com>
2023-06-07 15:15:27 +0200

    Implement getter and adder for collidable entities

--- src/systems/EntityManager.cpp
+++ src/systems/EntityManager.cpp
@@ -19,4 +19,21 @@ std::vector<Entity> EntityManager::getEntitesToRender() const {
   return m_renderableEntities;
 }
 
+std::vector<Entity> EntityManager::getCollidableEntites() const {
+  return m_collidableEntities;
+};
+
 Entity EntityManager::getCurrentSkyboxEntity() const { return m_currentSkybox; }
+
+void EntityManager::addRenderableEntity(Entity newRenderableEntity) {
+  m_renderableEntities.push_back(newRenderableEntity);
+}
+
+void EntityManager::addSkyboxEntity(Entity newSkyboxEntity) {
+  m_skyboxEntities.push_back(newSkyboxEntity);
+  m_currentSkybox = newSkyboxEntity;
+}
+
+void EntityManager::addCollidableEntity(Entity newCollidableEntity) {
+  m_collidableEntities.push_back(newCollidableEntity);
+}
