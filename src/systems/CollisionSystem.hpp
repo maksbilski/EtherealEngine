d9f850b217c677af9cd908566de96526db61d94b
Maks Biski <maksde25@gmail.com>
2023-06-08 23:35:09 +0200

    Add ray collision related declarations

--- src/systems/CollisionSystem.hpp
+++ src/systems/CollisionSystem.hpp
@@ -4,6 +4,11 @@
 #include "EntityManager.hpp"
 #include <memory>
 
+struct Ray {
+  glm::vec3 origin;
+  glm::vec3 direction;
+};
+
 class CollisionSystem {
 private:
   EntityManager &m_entityManager;
@@ -11,17 +16,26 @@ private:
   void transformCylinder(Cylinder &cylinder, const glm::vec3 &m_Position,
                          const glm::vec3 &m_Scale) const;
 
+  void transformSphere(Sphere &sphere, const glm::vec3 &m_Position,
+                       const glm::vec3 &m_Scale) const;
+
   std::optional<glm::vec3> checkCylinderCollision(const Cylinder &cylinder1,
                                                   const Cylinder &cylinder2,
                                                   Entity entity1,
                                                   Entity entity2) const;
 
+  bool checkRaySphereCollision(const Ray &ray, const Sphere &sphere) const;
+
   void handleCollision(const glm::vec3 &overlap, Entity entity1,
                        Entity entity2) const;
 
+  void handleRayCollision(Ray &ray, Entity entity) const;
+
   void controlEntitiesCollision() const;
 
-  // void controlRayCollision() const;
+  void controlRayCollision();
+
+  unsigned int m_rayHitCount = 0;
 
 public:
   CollisionSystem(EntityManager &EntityManger);
