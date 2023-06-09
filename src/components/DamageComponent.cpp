0a3afdd20e91134935463fdca782c20a0ad7c363
mrozek <01178582@pw.edu.pl>
2023-06-09 11:35:11 +0200

    removed useless damage component Xd

--- src/components/DamageComponent.cpp
+++
@@ -1,7 +0,0 @@
-#include "DamageComponent.hpp"
-
-DamageComponent::DamageComponent(int damage) : m_damage(damage) {}
-
-int DamageComponent::getDamage() const { return m_damage; }
-
-void DamageComponent::setDamage(int newDamage) { m_damage = newDamage; }
